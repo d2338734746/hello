1df7b7d6405179d46b3f1e02f01b987638cd53d3
d2338734746 <2338734746@qq.com>
2019-04-03 09:10:03 +0800

    edit

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php 
+$arr=[0,1,2,12,4,5,6];
+$m=5;
+ 
+function index($arr,$m){
+$start=0;
+$end=count($arr)-1;
 
+	 if ($m==$arr[$start]) {
+	 return $start;
+	 exit;
+	 }
+ if ($m==$arr[$end]) {
+	 return $end;
+	 exit;
+	 }
+while ($arr[$start]<=$arr[$end]) {
+        if ($arr[$start]>$m) {
+	 	 $start++;
+	 	 return $start;
+	 	 exit;
+	 	 }else if($arr[$end]<$m) {
+	 	 $end--;
+	 	  	 return $end;
+	 	  	 exit;
 
-2
-2
-21
-2121
-1
+	 	 }
+}
+	 
+	 	 
+}
+
+print_r(index($arr,$m));
  ?>
